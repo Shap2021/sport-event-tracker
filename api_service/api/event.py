4acb1a2be9f2167dfcf56b32e9b20bc1f3a0a64b
Shap2021 <71774396+Shap2021@users.noreply.github.com>
2025-08-20 21:33:57 -0400

    Update event.py
    Updating event route to support aysnc functionality. The event route now depends on the producer service to be running.

--- api_service/api/event.py
+++ api_service/api/event.py
@@ -4,7 +4,7 @@ Module for event route
 from fastapi import APIRouter, Depends, status
 
 from api_service.services.auth import get_current_user
-from api_service.services.producer import producer
+from api_service.core.utils import get_producer
 from api_service.schemas import (
     event,
     message
@@ -17,11 +17,15 @@ router = APIRouter(
 
 # Need to create a default response model and a pydantic schema of what that post event looks like
 @router.post("", status_code=status.HTTP_201_CREATED)
-def post_event(event: event.GameEvent, user = Depends(get_current_user)):
+async def post_event(
+    event: event.GameEvent, 
+    user = Depends(get_current_user), 
+    producer = Depends(get_producer)
+    ):
     """
     Post sports event payload
     """ 
-    producer.produce_message(
+    await producer.produce_message(
         key=f'event-key-{event.play_id}', #use the event id from the payload
         value=event
     )
